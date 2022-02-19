abc69c1ea477ecaff20c89694c82e30df54b5ec3
batumutsu <batumutsu@gmail.com>
2022-02-19 14:09:52 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, TextInput } from "react-native-web";
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    text: ''
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={styles.root}>
+      <Text>Battle Math</Text>
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
+      <Button
+        onPress={() => console.log(this.state.text)}
+        title="Submit"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple buttom"
+        />
+    </View>
   );
+  }
 }
+
+const styles = StyleSheet.create({
+ root: {
+   justifyContent: 'center',
+   alignItems: 'center'
+ }
+});
 
 export default App;
